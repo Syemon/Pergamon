9f7327819407c7539245c18d8c1c6ebac11303cd
szymon linowski <sz.linowski@gmail.com>
2019-05-08 16:58:17 +0200

    Initialize project

---
+++ src/main/java/com/pergamon/Pergamnon/PergamnonApplication.java
@@ -0,0 +1,13 @@
+package com.pergamon.Pergamnon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PergamnonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PergamnonApplication.class, args);
+	}
+
+}
